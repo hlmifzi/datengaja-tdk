0ed474ce92bec7782663a37473de79e706de2e86
helmi Fauzi <34008369+hlmifzi@users.noreply.github.com>
2020-12-12 15:12:19 +0700

    Add files via upload
    first commit

---
+++ next.config.js
@@ -0,0 +1,26 @@
+const withSass = require('@zeit/next-sass')
+module.exports = withSass((phase, { defaultConfig }) => {
+  const isDev = phase === PHASE_DEVELOPMENT_SERVER
+  const isProd = phase === PHASE_PRODUCTION_BUILD && process.env.STAGING !== '1'
+  const isStaging = phase === PHASE_PRODUCTION_BUILD && process.env.STAGING === '1'
+
+  const env = {
+    RESTURL_SPEAKERS: (() => {
+      if (isDev) return 'http://localhost:4000/speakers'
+      if (isProd) return 'https://www.siliconvalley-codecamp.com/rest/speakers/ps'
+      if (isStaging) return 'http://localhost:11639'
+      return 'RESTURL_SPEAKERS:not (isDev,isProd && !isStaging,isProd && isStaging)'
+    })(),
+    RESTURL_SESSIONS: (() => {
+      if (isDev) return 'http://localhost:4000/sessions'
+      if (isProd) return 'https://www.siliconvalley-codecamp.com/rest/sessions'
+      if (isStaging) return 'http://localhost:11639'
+      return 'RESTURL_SESSIONS:not (isDev,isProd && !isStaging,isProd && isStaging)'
+    })(),
+  }
+
+  return {
+    env,
+    cssModules: true
+  }
+})
