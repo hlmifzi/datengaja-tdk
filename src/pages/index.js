3c8ad5633877e42603708a53757fdec162f63b23
helmi_fauzi <helmi.fauzi@sirclo.com>
2021-04-04 18:14:00 +0700

    lazyload component and helmi jannah

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import Layout from '../components/Layout/Layout'
+import { LazyLoadComponent } from "react-lazy-load-image-component";
 import Link from "next/link";
 
 export default function Home() {
@@ -14,101 +15,100 @@ export default function Home() {
         <img src="img/undangan-nikah-home1.png" />
       </section>
 
-      <section className="container-fluid p-section home_template">
-        <img src="img/undangan-nikah-online-design-keren.png" />
-        <div className="home_promotionTemplate" >
-          <h1>Gak perlu mikirin design, kami menyediakan banyak <span>Design Keren</span> yang bebas anda pilih</h1>
-        </div>
-      </section>
+      <LazyLoadComponent>
+        <section className="container-fluid p-section home_template">
+          <img src="img/undangan-nikah-online-design-keren.png" />
+          <div className="home_promotionTemplate" >
+            <h1>Gak perlu mikirin design, kami menyediakan banyak <span>Design Keren</span> yang bebas anda pilih</h1>
+          </div>
+        </section>
+      </LazyLoadComponent>
 
-      <section className="home_portofolio">
-        <div className="d-flex">
-          <h2>Klien yang telah menggunakan jasa kami</h2>
-        </div>
+      <LazyLoadComponent>
+        <section className="home_portofolio">
+          <div className="d-flex">
+            <h2>Klien yang telah menggunakan jasa kami</h2>
+          </div>
 
-        <div className="home_portofolioContent p-section">
-
-          <div class="home_portofolioCard">
-            <h5 class="card-title">Awan & Pelangi</h5>
-            <img class="card-img-top" src="/img/design01.png" alt="Card image cap" />
-            <div class="card-body  text-center">
-              <h5 class="card-title line-through">Rp 200,000 - 400,000</h5>
-              <h5 class="card-title c_main font-weight-bold">GRATIS</h5>
-              <Link href="/design01" as={`/design01`}>
+          <div className="home_portofolioContent p-section">
+            <div class="home_portofolioCard">
+              <h5 class="card-title">Awan & Pelangi</h5>
+              <img class="card-img-top" src="/img/design01.png" alt="Card image cap" />
+              <div class="card-body  text-center">
+                <h5 class="card-title line-through">Rp 200,000 - 400,000</h5>
+                <h5 class="card-title c_main font-weight-bold">GRATIS</h5>
+                <Link href="/design01" as={`/design01`}>
+                  <button href="#" class="btn-main">Lihat Demo</button>
+                </Link>
+              </div>
+            </div>
+            <div class="home_portofolioCard">
+              <h5 class="card-title">Sulaiman dan Aisyah</h5>
+              <img class="card-img-top" src="/img/design02.png" alt="Card image cap" />
+              <div class="card-body text-center">
+                <h5 class="card-title line-through">Rp 200,000 - 400,000</h5>
+                <h5 class="card-title c_main font-weight-bold">GRATIS</h5>
+                <Link href="/design01" as={`/design01`}>
+                  <button href="#" class="btn-main">Lihat Demo</button>
+                </Link>
+              </div>
+            </div>
+            <div class="home_portofolioCard">
+              <h5 class="card-title">Josua & Martina</h5>
+              <img class="card-img-top" src="/img/design01.png" alt="Card image cap" />
+              <div class="card-body  text-center">
+                <h5 class="card-title line-through">Rp 200,000 - 400,000</h5>
+                <h5 class="card-title c_main font-weight-bold">GRATIS</h5>
+                <Link href="/design01" as={`/design01`}>
+                  <button href="#" class="btn-main">Lihat Demo</button>
+                </Link>
+              </div>
+            </div>
+            <div class="home_portofolioCard">
+              <h5 class="card-title">Helmi dan Jannah</h5>
+              <img class="card-img-top" src="/img/design02.png" alt="Card image cap" />
+              <div class="card-body text-center">
+                <h5 class="card-title line-through">Rp 200,000 - 400,000</h5>
+                <h5 class="card-title c_main font-weight-bold">GRATIS</h5>
+                <Link href="/design02" as={`/design02`}>
+                  <button href="#" class="btn-main">Lihat Demo</button>
+                </Link>
+              </div>
+            </div>
+            <div class="home_portofolioCard">
+              <h5 class="card-title">Awan & Pelangi</h5>
+              <img class="card-img-top" src="/img/design01.png" alt="Card image cap" />
+              <div class="card-body  text-center">
+                <h5 class="card-title line-through">Rp 200,000 - 400,000</h5>
+                <h5 class="card-title c_main font-weight-bold">GRATIS</h5>
+                <Link href="/design01" as={`/design01`}>
+                  <button href="#" class="btn-main">Lihat Demo</button>
+                </Link>
+              </div>
+            </div>
+            <div class="home_portofolioCard">
+              <h5 class="card-title">Sulaiman dan Aisyah</h5>
+              <img class="card-img-top" src="/img/design02.png" alt="Card image cap" />
+              <div class="card-body text-center">
+                <h5 class="card-title line-through">Rp 200,000 - 400,000</h5>
+                <h5 class="card-title c_main font-weight-bold">GRATIS</h5>
                 <button href="#" class="btn-main">Lihat Demo</button>
-              </Link>
+              </div>
             </div>
           </div>
+        </section>
+      </LazyLoadComponent>
 
-          <div class="home_portofolioCard">
-            <h5 class="card-title">Sulaiman dan Aisyah</h5>
-            <img class="card-img-top" src="/img/design02.png" alt="Card image cap" />
-            <div class="card-body text-center">
-              <h5 class="card-title line-through">Rp 200,000 - 400,000</h5>
-              <h5 class="card-title c_main font-weight-bold">GRATIS</h5>
-              <Link href="/design01" as={`/design01`}>
-                <button href="#" class="btn-main">Lihat Demo</button>
-              </Link>
-            </div>
-          </div>
-
-          <div class="home_portofolioCard">
-            <h5 class="card-title">Josua & Martina</h5>
-            <img class="card-img-top" src="/img/design01.png" alt="Card image cap" />
-            <div class="card-body  text-center">
-              <h5 class="card-title line-through">Rp 200,000 - 400,000</h5>
-              <h5 class="card-title c_main font-weight-bold">GRATIS</h5>
-              <Link href="/design01" as={`/design01`}>
-                <button href="#" class="btn-main">Lihat Demo</button>
-              </Link>
-            </div>
-          </div>
-
-          <div class="home_portofolioCard">
-            <h5 class="card-title">Slamet dan Sumiyem</h5>
-            <img class="card-img-top" src="/img/design02.png" alt="Card image cap" />
-            <div class="card-body text-center">
-              <h5 class="card-title line-through">Rp 200,000 - 400,000</h5>
-              <h5 class="card-title c_main font-weight-bold">GRATIS</h5>
-              <Link href="/design02" as={`/design02`}>
-                <button href="#" class="btn-main">Lihat Demo</button>
-              </Link>
-            </div>
-          </div>
-
-          <div class="home_portofolioCard">
-            <h5 class="card-title">Awan & Pelangi</h5>
-            <img class="card-img-top" src="/img/design01.png" alt="Card image cap" />
-            <div class="card-body  text-center">
-              <h5 class="card-title line-through">Rp 200,000 - 400,000</h5>
-              <h5 class="card-title c_main font-weight-bold">GRATIS</h5>
-              <Link href="/design01" as={`/design01`}>
-                <button href="#" class="btn-main">Lihat Demo</button>
-              </Link>
-            </div>
-          </div>
-
-          <div class="home_portofolioCard">
-            <h5 class="card-title">Sulaiman dan Aisyah</h5>
-            <img class="card-img-top" src="/img/design02.png" alt="Card image cap" />
-            <div class="card-body text-center">
-              <h5 class="card-title line-through">Rp 200,000 - 400,000</h5>
-              <h5 class="card-title c_main font-weight-bold">GRATIS</h5>
-              <button href="#" class="btn-main">Lihat Demo</button>
-            </div>
-          </div>
-
-        </div>
-      </section>
-
-      <section className="container-fluid home_createInvitation bg-grey-light p-section">
-        <h2>
-          Buat undangan nikah online Anda hanya dalam 1 menit
+      <LazyLoadComponent>
+        <section className="container-fluid home_createInvitation bg-grey-light p-section">
+          <h2>
+            Buat undangan nikah online Anda hanya dalam 1 menit
         </h2>
-        <Link href="/buat-undangan" as={`/buat-undangan`}>
-          <button className="btn-main">Buat Undangan Sekarang</button>
-        </Link>
-      </section>
+          <Link href="/buat-undangan" as={`/buat-undangan`}>
+            <button className="btn-main">Buat Undangan Sekarang</button>
+          </Link>
+        </section>
+      </LazyLoadComponent>
     </Layout >
   )
 }
