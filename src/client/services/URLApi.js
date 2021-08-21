167dadfa80519349bac47e2ba173191f5f5e0e19
helmi_fauzi <helmi.fauzi@sirclo.com>
2021-08-21 19:20:37 +0700

    url api

--- src/client/services/URLApi.js
+++ src/client/services/URLApi.js
@@ -6,7 +6,7 @@ import axios from 'axios';
 
 // export const developmentHost = '34.126.149.188/';
 export const developmentHost = 'http://localhost:3010/';
-export const productionHost = 'http://npm 34.126.149.188:3010/';
+export const productionHost = 'http://34.126.149.188:3010/';
 
 const ROOT_API = axios.create({
     baseURL: `${process.env.NODE_ENV === "development" ? developmentHost : productionHost}`,
