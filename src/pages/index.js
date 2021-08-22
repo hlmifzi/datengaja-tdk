8a5677d6b307a494e2bf0f9649bc299dd6fcc5db
helmi_fauzi <helmi.fauzi@sirclo.com>
2021-08-22 21:02:24 +0700

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,51 +21,45 @@ const Home = ({
         <img src="img/undangan-nikah-home1.png" />
       </section>
 
-      <LazyLoadComponent>
-        <section className="container-fluid p-section home_template">
-          <img src="img/undangan-nikah-online-design-keren.png" />
-          <div className="home_promotionTemplate" >
-            <h1>Gak perlu mikirin design, kami menyediakan banyak <span>Design Keren</span> yang bebas anda pilih</h1>
-          </div>
-        </section>
-      </LazyLoadComponent>
+      <section className="container-fluid p-section home_template">
+        <img src="img/undangan-nikah-online-design-keren.png" />
+        <div className="home_promotionTemplate" >
+          <h1>Gak perlu mikirin design, kami menyediakan banyak <span>Design Keren</span> yang bebas anda pilih</h1>
+        </div>
+      </section>
 
-      <LazyLoadComponent>
-        <section className="home_portofolio">
-          <div className="text-center">
-            <h2>Klien yang telah menggunakan jasa kami</h2>
-          </div>
+      <section className="home_portofolio">
+        <div className="text-center">
+          <h2>Klien yang telah menggunakan jasa kami</h2>
+        </div>
 
-          <div className="home_portofolioContent p-section">
-            {dataBuyerProducts?.map((v, i) => {
-              return (
-                <div class="home_portofolioCard" key={i}>
-                  <h5 class="card-title">{v.bridegroom_call_name} & {v.bride_call_name}</h5>
-                  <img class="card-img-top" src="/img/design01.png" alt="Card image cap" />
-                  <div class="card-body  text-center">
-                    <h5 class="card-title line-through">Rp 200,000 - 400,000</h5>
-                    <h5 class="card-title c_main font-weight-bold">GRATIS</h5>
-                    <Link href={`/undangan01/pernikahan-${v.bridegroom_call_name}-dan-${v.bride_call_name}`}>
-                      <button href="#" class="btn-main">Lihat Demo</button>
-                    </Link>
-                  </div>
+        <div className="home_portofolioContent p-section">
+          {dataBuyerProducts?.map((v, i) => {
+            return (
+              <div class="home_portofolioCard" key={i}>
+                <h5 class="card-title">{v.bridegroom_call_name} & {v.bride_call_name}</h5>
+                <img class="card-img-top" src="/img/design01.png" alt="Card image cap" />
+                <div class="card-body  text-center">
+                  <h5 class="card-title line-through">Rp 200,000 - 400,000</h5>
+                  <h5 class="card-title c_main font-weight-bold">GRATIS</h5>
+                  <Link href={`/undangan01/pernikahan-${v.bridegroom_call_name}-dan-${v.bride_call_name}`}>
+                    <button href="#" class="btn-main">Lihat Demo</button>
+                  </Link>
                 </div>
-              )
-            })}
-          </div>
-        </section>
-      </LazyLoadComponent>
+              </div>
+            )
+          })}
+        </div>
+      </section>
 
-      <LazyLoadComponent>
-        <section className="container-fluid home_createInvitation bg-grey-light p-section">
-          <h2>
-            Buat undangan nikah online Anda hanya dalam 1 menit
-          </h2>
-          <Link href="/buat-undangan" as={`/buat-undangan`}>
-            <button className="btn-main">Buat Undangan Sekarang</button>
-          </Link>
-        </section>
-      </LazyLoadComponent>
+      <section className="container-fluid home_createInvitation bg-grey-light p-section">
+        <h2>
+          Buat undangan nikah online Anda hanya dalam 1 menit
+        </h2>
+        <Link href="/buat-undangan" as={`/buat-undangan`}>
+          <button className="btn-main">Buat Undangan Sekarang</button>
+        </Link>
+      </section>
 
     </Layout >
   )
